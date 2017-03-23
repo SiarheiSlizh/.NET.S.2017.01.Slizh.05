ebcdaa17f2f4d285c5bdee0735c1c34c9c8de48b
сергей слиж <sergeislizh@mail.ru>
2017-03-23 18:43:19 +0300

    add Task2

--- Task2Tests/Task2Tests.cs
+++ Task2Tests/Task2Tests.cs
@@ -4,17 +4,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Task2;
 
 namespace Task2Tests
 {
     [TestFixture]
     public class Task2Tests
     {
-        [Test]
-        public void TestMethod()
+        [TestCase(-255.255, ExpectedResult = "1100000001101111111010000010100011110101110000101000111101011100")]
+        [TestCase(255.255, ExpectedResult = "0100000001101111111010000010100011110101110000101000111101011100")]
+        [TestCase(4294967295.0, ExpectedResult = "0100000111101111111111111111111111111111111000000000000000000000")]
+        [TestCase(double.MinValue, ExpectedResult = "1111111111101111111111111111111111111111111111111111111111111111")]
+        [TestCase(double.MaxValue, ExpectedResult = "0111111111101111111111111111111111111111111111111111111111111111")]
+        [TestCase(double.Epsilon, ExpectedResult = "0001")]
+        [TestCase(double.NaN, ExpectedResult = "1111111111111000000000000000000000000000000000000000000000000000")]
+        [TestCase(double.NegativeInfinity, ExpectedResult = "1111111111110000000000000000000000000000000000000000000000000000")]
+        [TestCase(double.PositiveInfinity, ExpectedResult = "0111111111110000000000000000000000000000000000000000000000000000")]
+
+        public string BitOperations_PositiveTests(double number)
         {
-            // TODO: Add your test code here
-            Assert.Pass("Your first passing test");
+            return number.GetBinaryNumber();
         }
     }
 }
